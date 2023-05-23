9ccbdfeab8844a004c91a0352f4f0cc4a35f4867
Haleshot <hari.leo03@gmail.com>
2023-05-23 11:33:06 +0530

    Updated Directory

--- All_Project_Files/Final_Project_Files/Down_Sampling_Image_Screen.py
+++ All_Project_Files/Final_Project_Files/Down_Sampling_Image_Screen.py
@@ -104,9 +104,9 @@ class Ui_Dialog(object):
             m, n, c = image_downsize.shape
             print("The new size of the image is ", m, " x ", n)
 
-
-            cv2.imwrite(r"Image_Processing\All_Project_Files\Final_Project_Files\Cam_Media\Down_Sized_Img\Down_Sized_Image.png", image_downsize)
-            Downsized_File_Name = r"Image_Processing\All_Project_Files\Final_Project_Files\Cam_Media\Down_Sized_Img\Down_Sized_Image.png"
+            
+            cv2.imwrite(r"All_Project_Files\Final_Project_Files\Cam_Media\Down_Sized_Img\Down_Sized_Image.png", image_downsize)
+            Downsized_File_Name = r"All_Project_Files\Final_Project_Files\Cam_Media\Down_Sized_Img\Down_Sized_Image.png"
             self.label_2.setPixmap(QPixmap(Downsized_File_Name))
 
 
