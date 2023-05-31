f89f5d11dbdcb4de199924d98ddc069180033d83
Haleshot <hari.leo03@gmail.com>
2023-05-31 10:33:41 +0530

    Updated

--- All_Project_Files/Final_Project_Files/Down_Sampling_Image_Screen.py
+++ All_Project_Files/Final_Project_Files/Down_Sampling_Image_Screen.py
@@ -137,7 +137,7 @@ class Ui_Dialog(object):
             self.label_5.setText("")
             image_downsize = cv2.imread(r"All_Project_Files\Final_Project_Files\Cam_Media\Down_Sized_Img\Down_Sized_Image.png")
             option = QFileDialog.Options()
-            save_as_path = QFileDialog.getSaveFileName(None, 'Open Image File', r"Image", "Image files (*.jpg *.jpeg *.gif *.png)")
+            save_as_path = QFileDialog.getSaveFileName(None, 'Open Image File', r"Down Sized Image", "Image files (*.jpg *.jpeg *.gif *.png)")
             print(save_as_path[0])
             cv2.imwrite(save_as_path[0], image_downsize)
         else:
