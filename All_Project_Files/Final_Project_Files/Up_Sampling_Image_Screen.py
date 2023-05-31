a2ccf24fed8a633a21c6abd5a105743f75af811b
Haleshot <hari.leo03@gmail.com>
2023-05-31 10:25:18 +0530

    Updated Up Sampling

--- All_Project_Files/Final_Project_Files/Up_Sampling_Image_Screen.py
+++ All_Project_Files/Final_Project_Files/Up_Sampling_Image_Screen.py
@@ -9,39 +9,14 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5 import QtCore, QtGui, QtWidgets, uic
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from PyQt5.uic import loadUi
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QStackedWidget, QComboBox, QPushButton, QVBoxLayout, QLabel
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QFileDialog
-import cv2
 
 
-class Ui_Dialog_1(object):
+class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         Dialog.resize(1366, 800)
-        self.Open_Image_Button = QtWidgets.QPushButton(Dialog)
-        self.Open_Image_Button.setGeometry(QtCore.QRect(70, 620, 401, 81))
-        self.Open_Image_Button.setObjectName("Open_Image_Button")
-        self.label_4 = QtWidgets.QLabel(Dialog)
-        self.label_4.setGeometry(QtCore.QRect(870, 640, 281, 41))
-        self.label_4.setStyleSheet("font: 16pt \"MS Shell Dlg 2\";")
-        self.label_4.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_4.setObjectName("label_4")
-        self.label_3 = QtWidgets.QLabel(Dialog)
-        self.label_3.setGeometry(QtCore.QRect(620, 290, 61, 31))
-        self.label_3.setStyleSheet("font: 22pt \"MS Shell Dlg 2\";")
-        self.label_3.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_3.setObjectName("label_3")
         self.lineEdit = QtWidgets.QLineEdit(Dialog)
-        self.lineEdit.setGeometry(QtCore.QRect(530, 620, 301, 81))
+        self.lineEdit.setGeometry(QtCore.QRect(550, 660, 301, 81))
         self.lineEdit.setStyleSheet("border: 12px  solid rgb(0, 0, 0);\n"
 "border-width: 2px;\n"
 "font: 11.5pt \"MS Shell Dlg 2\";\n"
@@ -50,19 +25,47 @@ class Ui_Dialog_1(object):
 "padding-right:20px;")
         self.lineEdit.setText("")
         self.lineEdit.setObjectName("lineEdit")
-        self.label_2 = QtWidgets.QLabel(Dialog)
-        self.label_2.setGeometry(QtCore.QRect(710, 20, 631, 561))
+        self.label_5 = QtWidgets.QLabel(Dialog)
+        self.label_5.setGeometry(QtCore.QRect(400, 740, 581, 41))
+        self.label_5.setStyleSheet("color: rgb(255, 0, 0);")
+        self.label_5.setText("")
+        self.label_5.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_5.setObjectName("label_5")
+        self.Save_As = QtWidgets.QPushButton(Dialog)
+        self.Save_As.setGeometry(QtCore.QRect(900, 660, 403, 81))
+        self.Save_As.setObjectName("Save_As")
+        self.scrollArea_2 = QtWidgets.QScrollArea(Dialog)
+        self.scrollArea_2.setGeometry(QtCore.QRect(730, 40, 571, 561))
+        self.scrollArea_2.setWidgetResizable(True)
+        self.scrollArea_2.setObjectName("scrollArea_2")
+        self.scrollAreaWidgetContents_2 = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents_2.setGeometry(QtCore.QRect(0, 0, 569, 559))
+        self.scrollAreaWidgetContents_2.setObjectName("scrollAreaWidgetContents_2")
+        self.label_2 = QtWidgets.QLabel(self.scrollAreaWidgetContents_2)
+        self.label_2.setGeometry(QtCore.QRect(10, 20, 571, 561))
         self.label_2.setText("")
         self.label_2.setObjectName("label_2")
-        self.label = QtWidgets.QLabel(Dialog)
+        self.scrollArea_2.setWidget(self.scrollAreaWidgetContents_2)
+        self.Open_Image_Button = QtWidgets.QPushButton(Dialog)
+        self.Open_Image_Button.setGeometry(QtCore.QRect(90, 660, 401, 81))
+        self.Open_Image_Button.setObjectName("Open_Image_Button")
+        self.scrollArea = QtWidgets.QScrollArea(Dialog)
+        self.scrollArea.setGeometry(QtCore.QRect(80, 40, 571, 561))
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setObjectName("scrollArea")
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 569, 559))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.label = QtWidgets.QLabel(self.scrollAreaWidgetContents)
         self.label.setGeometry(QtCore.QRect(10, 20, 571, 561))
         self.label.setText("")
         self.label.setObjectName("label")
-        self.label_5 = QtWidgets.QLabel(Dialog)
-        self.label_5.setGeometry(QtCore.QRect(410, 730, 581, 41))
-        self.label_5.setStyleSheet("color: rgb(255, 0, 0);")
-        self.label_5.setText("")
-        self.label_5.setObjectName("label_5")
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.label_3 = QtWidgets.QLabel(Dialog)
+        self.label_3.setGeometry(QtCore.QRect(660, 300, 61, 31))
+        self.label_3.setStyleSheet("font: 22pt \"MS Shell Dlg 2\";")
+        self.label_3.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_3.setObjectName("label_3")
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
@@ -70,62 +73,17 @@ class Ui_Dialog_1(object):
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.Open_Image_Button.setText(_translate("Dialog", "Open Image"))
-        self.label_4.setText(_translate("Dialog", "Output Image"))
-        self.label_3.setText(_translate("Dialog", "=>"))
         self.lineEdit.setPlaceholderText(_translate("Dialog", "Enter Up Sampling Value..."))
-
-
-
-        self.Open_Image_Button.clicked.connect(self.File_Select)
-
-
-    def File_Select(self):
-        Up_Sampling_Value = self.lineEdit.text() # Accessing the value entered by the user.
-        
-        if not (int(Up_Sampling_Value.isdigit())):
-            self.label_5.setText("Please enter an integer value!")
-        else:
-            self.label_5.setText("")
-            # fname = QFileDialog.getOpenFileName(self, "Open File", "All_Project_Files\Final_Project_Files\Cam_Media", "Images (*.png *.xpm *.jpg)")
-            # # Opening the Image
-            # self.pixmap = QPixmap(fname[0]) # This returns a tuple and hence we mention [0].
-            # # Adding the picture to the Label.
-            # self.label.setPixmap(self.pixmap)
-            Up_Sampling_Value = int(self.lineEdit.text())
-            file_name, _ = QFileDialog.getOpenFileName(None, 'Open Image File', r"<Default dir>", "Image files (*.jpg *.jpeg *.gif *.png)")
-            self.label.setPixmap(QPixmap(file_name))
-            img = cv2.imread(file_name)
-            m, n, c = img.shape
-            print("The original size of the image is ", m, " x ", n)
-            img_double = cv2.resize(img, (0, 0), fx = Up_Sampling_Value, fy = Up_Sampling_Value)
-            m, n, c = img_double.shape
-            print("The new size of the image is ", m, " x ", n)
-
-
-            cv2.imwrite(r"All_Project_Files\Final_Project_Files\Cam_Media\Up_Sized_Img\Up_Sized_Image.png", img_double)
-            Up_File_Name = r"All_Project_Files\Final_Project_Files\Cam_Media\Up_Sized_Img\Up_Sized_Image.png"
-            
-            
-            self.label_2.setPixmap(QPixmap(Up_File_Name))
-
-
-            # If you want these to display these in separate windows other than GUI.
-            # cv2.imshow("Negative Image", negative_img)
-
-
-            # cv2.imshow("Image", img)
-            # cv2.waitKey(0)
-    
-            # # closing all open windows
-            # cv2.destroyAllWindows()
+        self.Save_As.setText(_translate("Dialog", "Save As"))
+        self.Open_Image_Button.setText(_translate("Dialog", "Open Image"))
+        self.label_3.setText(_translate("Dialog", "=>"))
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog_1()
+    ui = Ui_Dialog()
     ui.setupUi(Dialog)
     Dialog.show()
     sys.exit(app.exec_())
---
+++ All_Project_Files/Final_Project_Files/Up_Sampling_Image_Screen_ui.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'Up_Sampling_Image_Screen.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
+        Dialog.resize(1366, 800)
+        self.lineEdit = QLineEdit(Dialog)
+        self.lineEdit.setObjectName(u"lineEdit")
+        self.lineEdit.setGeometry(QRect(550, 660, 301, 81))
+        self.lineEdit.setStyleSheet(u"border: 12px  solid rgb(0, 0, 0);\n"
+"border-width: 2px;\n"
+"font: 11.5pt \"MS Shell Dlg 2\";\n"
+"border-radius: 15px;\n"
+"padding-left:20px;\n"
+"padding-right:20px;")
+        self.label_5 = QLabel(Dialog)
+        self.label_5.setObjectName(u"label_5")
+        self.label_5.setGeometry(QRect(400, 740, 581, 41))
+        self.label_5.setStyleSheet(u"color: rgb(255, 0, 0);")
+        self.label_5.setAlignment(Qt.AlignCenter)
+        self.Save_As = QPushButton(Dialog)
+        self.Save_As.setObjectName(u"Save_As")
+        self.Save_As.setGeometry(QRect(900, 660, 403, 81))
+        self.scrollArea_2 = QScrollArea(Dialog)
+        self.scrollArea_2.setObjectName(u"scrollArea_2")
+        self.scrollArea_2.setGeometry(QRect(730, 40, 571, 561))
+        self.scrollArea_2.setWidgetResizable(True)
+        self.scrollAreaWidgetContents_2 = QWidget()
+        self.scrollAreaWidgetContents_2.setObjectName(u"scrollAreaWidgetContents_2")
+        self.scrollAreaWidgetContents_2.setGeometry(QRect(0, 0, 569, 559))
+        self.label_2 = QLabel(self.scrollAreaWidgetContents_2)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(10, 20, 571, 561))
+        self.scrollArea_2.setWidget(self.scrollAreaWidgetContents_2)
+        self.Open_Image_Button = QPushButton(Dialog)
+        self.Open_Image_Button.setObjectName(u"Open_Image_Button")
+        self.Open_Image_Button.setGeometry(QRect(90, 660, 401, 81))
+        self.scrollArea = QScrollArea(Dialog)
+        self.scrollArea.setObjectName(u"scrollArea")
+        self.scrollArea.setGeometry(QRect(80, 40, 571, 561))
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollAreaWidgetContents = QWidget()
+        self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
+        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 569, 559))
+        self.label = QLabel(self.scrollAreaWidgetContents)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(10, 20, 571, 561))
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.label_3 = QLabel(Dialog)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(660, 300, 61, 31))
+        self.label_3.setStyleSheet(u"font: 22pt \"MS Shell Dlg 2\";")
+        self.label_3.setAlignment(Qt.AlignCenter)
+
+        self.retranslateUi(Dialog)
+
+        QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Dialog", None))
+        self.lineEdit.setText("")
+        self.lineEdit.setPlaceholderText(QCoreApplication.translate("Dialog", u"Enter Up Sampling Value...", None))
+        self.label_5.setText("")
+        self.Save_As.setText(QCoreApplication.translate("Dialog", u"Save As", None))
+        self.label_2.setText("")
+        self.Open_Image_Button.setText(QCoreApplication.translate("Dialog", u"Open Image", None))
+        self.label.setText("")
+        self.label_3.setText(QCoreApplication.translate("Dialog", u"=>", None))
+    # retranslateUi
+
