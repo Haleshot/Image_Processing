a25f7edf26ab0b4f7fb35531c90cf70576720ef8
Haleshot <hari.leo03@gmail.com>
2023-05-31 12:27:07 +0530

    Updated

--- All_Project_Files/Final_Project_Files/Gaussian_Image_Screen.py
+++ All_Project_Files/Final_Project_Files/Gaussian_Image_Screen.py
@@ -1,7 +1,4 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
@@ -15,37 +12,51 @@ import cv2
 from skimage.util import random_noise
 import numpy as np
 
-
 class Ui_Dialog_6(object):
     def setupUi(self, Dialog_6):
         Dialog_6.setObjectName("Dialog_6")
         Dialog_6.resize(1366, 800)
-        self.label_2 = QtWidgets.QLabel(Dialog_6)
-        self.label_2.setGeometry(QtCore.QRect(690, 20, 571, 561))
-        self.label_2.setText("")
-        self.label_2.setObjectName("label_2")
-        self.Open_Image_Button = QtWidgets.QPushButton(Dialog_6)
-        self.Open_Image_Button.setGeometry(QtCore.QRect(70, 620, 401, 81))
-        self.Open_Image_Button.setObjectName("Open_Image_Button")
-        self.label = QtWidgets.QLabel(Dialog_6)
-        self.label.setGeometry(QtCore.QRect(10, 20, 571, 561))
-        self.label.setText("")
-        self.label.setObjectName("label")
-        self.label_4 = QtWidgets.QLabel(Dialog_6)
-        self.label_4.setGeometry(QtCore.QRect(870, 640, 281, 41))
-        self.label_4.setStyleSheet("font: 16pt \"MS Shell Dlg 2\";")
-        self.label_4.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_4.setObjectName("label_4")
-        self.label_5 = QtWidgets.QLabel(Dialog_6)
-        self.label_5.setGeometry(QtCore.QRect(410, 730, 581, 41))
-        self.label_5.setStyleSheet("color: rgb(255, 0, 0);")
-        self.label_5.setText("")
-        self.label_5.setObjectName("label_5")
+        self.Save_As = QtWidgets.QPushButton(Dialog_6)
+        self.Save_As.setGeometry(QtCore.QRect(900, 660, 403, 81))
+        self.Save_As.setObjectName("Save_As")
         self.label_3 = QtWidgets.QLabel(Dialog_6)
-        self.label_3.setGeometry(QtCore.QRect(610, 290, 61, 31))
+        self.label_3.setGeometry(QtCore.QRect(660, 300, 61, 31))
         self.label_3.setStyleSheet("font: 22pt \"MS Shell Dlg 2\";")
         self.label_3.setAlignment(QtCore.Qt.AlignCenter)
         self.label_3.setObjectName("label_3")
+        self.label_5 = QtWidgets.QLabel(Dialog_6)
+        self.label_5.setGeometry(QtCore.QRect(400, 740, 581, 41))
+        self.label_5.setStyleSheet("color: rgb(255, 0, 0);")
+        self.label_5.setText("")
+        self.label_5.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_5.setObjectName("label_5")
+        self.scrollArea_2 = QtWidgets.QScrollArea(Dialog_6)
+        self.scrollArea_2.setGeometry(QtCore.QRect(730, 40, 571, 561))
+        self.scrollArea_2.setWidgetResizable(True)
+        self.scrollArea_2.setObjectName("scrollArea_2")
+        self.scrollAreaWidgetContents_2 = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents_2.setGeometry(QtCore.QRect(0, 0, 569, 559))
+        self.scrollAreaWidgetContents_2.setObjectName("scrollAreaWidgetContents_2")
+        self.label_2 = QtWidgets.QLabel(self.scrollAreaWidgetContents_2)
+        self.label_2.setGeometry(QtCore.QRect(10, 20, 571, 561))
+        self.label_2.setText("")
+        self.label_2.setObjectName("label_2")
+        self.scrollArea_2.setWidget(self.scrollAreaWidgetContents_2)
+        self.Open_Image_Button = QtWidgets.QPushButton(Dialog_6)
+        self.Open_Image_Button.setGeometry(QtCore.QRect(90, 660, 401, 81))
+        self.Open_Image_Button.setObjectName("Open_Image_Button")
+        self.scrollArea = QtWidgets.QScrollArea(Dialog_6)
+        self.scrollArea.setGeometry(QtCore.QRect(80, 40, 571, 561))
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setObjectName("scrollArea")
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 569, 559))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.label = QtWidgets.QLabel(self.scrollAreaWidgetContents)
+        self.label.setGeometry(QtCore.QRect(10, 20, 571, 561))
+        self.label.setText("")
+        self.label.setObjectName("label")
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
 
         self.retranslateUi(Dialog_6)
         QtCore.QMetaObject.connectSlotsByName(Dialog_6)
