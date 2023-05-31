d1c50841da413df813588f0117122eb7ef614884
Haleshot <hari.leo03@gmail.com>
2023-05-31 11:46:24 +0530

    Updated

--- All_Project_Files/Final_Project_Files/Thresholding_Without_Background.py
+++ All_Project_Files/Final_Project_Files/Thresholding_Without_Background.py
@@ -180,7 +180,7 @@ class Ui_Dialog_2(object):
             self.label_5.setText("")
             image_downsize = cv2.imread(r"All_Project_Files\Final_Project_Files\Cam_Media\Thresholding_Without\Threshold_Without_Image.png")
             option = QFileDialog.Options()
-            save_as_path = QFileDialog.getSaveFileName(None, 'Open Image File', r"Down Sized Image", "Image files (*.jpg *.jpeg *.gif *.png)")
+            save_as_path = QFileDialog.getSaveFileName(None, 'Open Image File', r"Thresholding Without Background", "Image files (*.jpg *.jpeg *.gif *.png)")
 
             cv2.imwrite(save_as_path[0], image_downsize)
         else:
