8c3e06f891db119047484f14cc6acfc98233b723
Haleshot <hari.leo03@gmail.com>
2023-05-31 13:23:29 +0530

    Update UI laytout and py corresponding file

--- All_Project_Files/Final_Project_Files/Image_Processing_Options.py
+++ All_Project_Files/Final_Project_Files/Image_Processing_Options.py
@@ -1,6 +1,26 @@
-
-
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.uic import loadUi
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QStackedWidget, QComboBox, QPushButton, QVBoxLayout, QLabel
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QFileDialog
+
+
+# import files of various UI pages used in the Project (Workflow)
+from Negative_Image_Screen import Ui_Background
+from Down_Sampling_Image_Screen import Ui_Dialog
+from Up_Sampling_Image_Screen import Ui_Dialog_1
+from Thresholding_Without_Background import Ui_Dialog_2
+from Thresholding_With_Background import Ui_Dialog_3
+from Blur_Image_Screen import Ui_Dialog_4
+from LPF_Image_Screen import Ui_Dialog_5
+from Gaussian_Image_Screen import Ui_Dialog_6
+from Facial_Features_Image_Screen import Ui_Dialog_7
+from Laplace_Screen import Ui_Dialog_8
 
 
 class Ui_Image_Processing_Options(object):
@@ -91,6 +111,130 @@ class Ui_Image_Processing_Options(object):
         self.Title.setText(_translate("Image_Processing_Options", "Image Processing Options:"))
         self.Facial_Feature_Detection_Button.setText(_translate("Image_Processing_Options", "Facial Feature Detection"))
         self.Gaussian_Button_2.setText(_translate("Image_Processing_Options", "Laplace Edge"))
+
+
+        # When user clicks the Down Sampling button:
+        self.DownSampling_Button.clicked.connect(self.DownSamplingFunction)
+
+        # When user clicks the Up Sampling button:
+        self.UpSampling_Button.clicked.connect(self.UpSamplingFunction)
+
+        # When user clicks the Down Sampling button:
+        self.Negative_Button.clicked.connect(self.NegativeFunction)
+
+        # When user clicks the Down Sampling button:
+        self.Threshold_With_Button.clicked.connect(self.Threshold_WithFunction)
+
+        # When user clicks the Down Sampling button:
+        self.Threshold_Without_Button.clicked.connect(self.Threshold_WithoutFunction)
+
+        # When user clicks the Down Sampling button:
+        self.Blur_Button.clicked.connect(self.Blur_Function)
+
+        # When user clicks the Down Sampling button:
+        self.LPF_Button.clicked.connect(self.LPF_Function)
+
+        # When user clicks the Down Sampling button:
+        self.Gaussian_Button.clicked.connect(self.Gaussian_Function)
+
+        # When user clicks the Facial_Feature_Detection_Button button:
+        self.Facial_Feature_Detection_Button.clicked.connect(self.Facial_Feature_Detection_Function)
+
+        # When user clicks the Laplace button:
+        self.Laplace_Button.clicked.connect(self.Laplace_Function)
+
+
+
+    def DownSamplingFunction(self):
+        self.window = QtWidgets.QMainWindow()
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self.window)
+
+        # When running in this file, it is working but running the Main File prompts the error - Background is not defined.
+        self.window.show()
+
+    def UpSamplingFunction(self):
+        self.window = QtWidgets.QMainWindow()
+        self.ui = Ui_Dialog_1()
+        self.ui.setupUi(self.window)
+
+        # When running in this file, it is working but running the Main File prompts the error - Background is not defined.
+        self.window.show()
+
+    def NegativeFunction(self):
+
+        self.window = QtWidgets.QMainWindow()
+        self.ui = Ui_Background()
+        self.ui.setupUi(self.window)
+
+        # When running in this file, it is working but running the Main File prompts the error - Background is not defined.
+        self.window.show()
+
+
+
+
+    def Threshold_WithFunction(self):
+        
+        self.window = QtWidgets.QMainWindow()
+        self.ui = Ui_Dialog_3()
+        self.ui.setupUi(self.window)
+
+        # When running in this file, it is working but running the Main File prompts the error - Background is not defined.
+        self.window.show()
+
+    def Threshold_WithoutFunction(self):
+        
+        self.window = QtWidgets.QMainWindow()
+        self.ui = Ui_Dialog_2()
+        self.ui.setupUi(self.window)
+
+        # When running in this file, it is working but running the Main File prompts the error - Background is not defined.
+        self.window.show()
+
+    def Blur_Function(self):
+        
+        self.window = QtWidgets.QMainWindow()
+        self.ui = Ui_Dialog_4()
+        self.ui.setupUi(self.window)
+
+        # When running in this file, it is working but running the Main File prompts the error - Background is not defined.
+        self.window.show()
+
+    def LPF_Function(self):
+        
+        self.window = QtWidgets.QMainWindow()
+        self.ui = Ui_Dialog_5()
+        self.ui.setupUi(self.window)
+
+        # When running in this file, it is working but running the Main File prompts the error - Background is not defined.
+        self.window.show()
+
+    def Gaussian_Function(self):
+        
+        self.window = QtWidgets.QMainWindow()
+        self.ui = Ui_Dialog_6()
+        self.ui.setupUi(self.window)
+
+        # When running in this file, it is working but running the Main File prompts the error - Background is not defined.
+        self.window.show()
+
+    def Facial_Feature_Detection_Function(self):
+        
+        self.window = QtWidgets.QMainWindow()
+        self.ui = Ui_Dialog_7()
+        self.ui.setupUi(self.window)
+
+        # When running in this file, it is working but running the Main File prompts the error - Background is not defined.
+        self.window.show()
+
+    def Laplace_Function(self):
+        
+        self.window = QtWidgets.QMainWindow()
+        self.ui = Ui_Dialog_8()
+        self.ui.setupUi(self.window)
+
+        # When running in this file, it is working but running the Main File prompts the error - Background is not defined.
+        self.window.show()
 
 
 if __name__ == "__main__":
