b463990d16cc6ef2fa76dca1b198b5b2a9bdc7f0
Haleshot <hari.leo03@gmail.com>
2023-07-27 12:48:39 +0530

    Add Sphinx documentation support

--- All_Project_Files/Final_Project_Files/docs/conf.py
+++ All_Project_Files/Final_Project_Files/docs/conf.py
@@ -19,7 +19,11 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
